395d0e85fb4f49f3e65d60d3120bef3140613368
David <david.norton@cgi.com>
2018-11-01 14:14:13 +0000

    Configure ngrx with devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material/material.module';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
-import { reducers } from './store/app.reducers';
-import { EffectsModule } from '@ngrx/effects';
+import { metaReducers, reducers } from './store/app.reducers';
 import { AuthEffects } from './core/auth/store/auth.effects';
 import { AuthInterceptor } from './core/auth/auth.interceptor';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './core/header/header.component';
-
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { HeaderComponent } from './core/header/header.component';
     AppRoutingModule,
     HttpClientModule,
     MaterialModule,
-    StoreModule.forRoot(reducers),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument()  : [],
+    StoreRouterConnectingModule.forRoot({stateKey: 'router'}),
     EffectsModule.forRoot([ AuthEffects ]),
     StoreRouterConnectingModule
   ],
--- src/app/store/app.reducers.ts
+++ src/app/store/app.reducers.ts
@@ -1,11 +1,19 @@
-import { ActionReducerMap } from '@ngrx/store';
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 
 import * as fromAuth from '../core/auth/store/auth.reducers';
+import { environment } from '../../environments/environment';
+import { storeFreeze } from 'ngrx-store-freeze';
+import { routerReducer } from '@ngrx/router-store';
 
 export interface AppState {
+  router: any;
   activeUser: fromAuth.State;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
+  router: routerReducer,
   activeUser: fromAuth.authReducer
 };
+
+export const metaReducers: MetaReducer<AppState>[] =
+  !environment.production ? [storeFreeze] : [];
